f4a49ecf7c239682f2323eb929a7e85e6ddfa552
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-08-28 03:38:25 +0700

    feat: add react-icons dependency and update button component; include new assets and modify CSS variables

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button';
+
 function App() {
   return (
     <div>
       <h1>Montserrat</h1>
       <h2 className='font-anton'>Anton</h2>
       <h2 className='font-bonheurRoyale'>BonheurRoyale</h2>
+      <Button>Let's Talk</Button>
     </div>
   );
 }
