1c48ee608b59440f54ca057c126b966c68247316
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-08-28 01:50:53 +0700

    feat: initialize personal project with React, Vite, and Tailwind CSS
    - Add package.json with dependencies and scripts for development, build, and linting.
    - Create main application component (App.tsx) with Montserrat and Anton fonts.
    - Include font files for Montserrat, Anton, and BonheurRoyale.
    - Implement Accordion, Button, and Carousel components using Radix UI and Embla Carousel.
    - Add utility functions for class name merging.
    - Set up main entry point (main.tsx) and global styles (index.css) with Tailwind CSS.
    - Configure TypeScript with appropriate settings for app and node environments.
    - Set up Vite configuration for React and Tailwind CSS integration.

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Montserrat</h1>
+      <h2 className='font-anton'>Anton</h2>
+      <h2 className='font-bonheurRoyale'>BonheurRoyale</h2>
+    </div>
+  );
+}
+
+export default App;
