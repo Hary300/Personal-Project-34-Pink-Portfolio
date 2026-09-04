c188bb259bb9473fb4068d0cf62a4bb2620ba270
Haryadi Nugraha Putra <h.nugraha.p@gmail.com>
2026-09-04 15:29:00 +0700

    feat: add minimum height to HeroSection for improved layout consistency

--- src/sections/01-hero/HeroSection.tsx
+++ src/sections/01-hero/HeroSection.tsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
   return (
     <SectionWrapper
       sectionId='home'
-      className='lg:relative flex flex-col justify-center items-center pt-22 lg:pt-56.75 gap-9.5 pb-0 sm:gap-4 sm:flex-row sm:items-stretch lg:items-end lg:pb-0'
+      className='lg:relative flex flex-col justify-center items-center pt-22 lg:pt-56.75 gap-9.5 pb-0 sm:gap-4 sm:flex-row sm:items-stretch lg:items-end lg:pb-0  lg:min-h-256'
       style={{
         backgroundImage: `linear-gradient(rgba(165, 56, 96, 0.94), rgba(165, 56, 96, 0.9)), url(${pattern})`,
         backgroundSize: 'cover',
